1313938081091422a16a35f61137c490cf2f9a88
Alex <alexbachurin91@gmail.com>
2021-09-24 15:25:46 +0300

    pages structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import HomePage from "./pages/HomePage";
 import MarkdownPage from "./pages/MarkdownPage";
+import Navbar from "./components/Navbar";
 function App() {
   const [markdown, setMarkdown] = useState('## markdown preview')
   return (
     <>
+      <Navbar />
       <HomePage />
       <MarkdownPage markdown={markdown} setMarkdown={setMarkdown} />
     </>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-
+        <div className='home'>
+            <h1>Welcome to markdown editor!</h1>
+            <div className="home-center">
+                <p>click button to proceed to editor</p>
+                <button>Go to editor</button>
+            </div>
         </div>
     )
 }
