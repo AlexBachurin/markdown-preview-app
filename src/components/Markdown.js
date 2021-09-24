718225eae2be02c74b3dcb61f52f07bc298d6719
Alex <alexbachurin91@gmail.com>
2021-09-24 15:02:48 +0300

    working markdown with help of react-markdown

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import ReactMarkdown from 'react-markdown'
 const Markdown = ({ markdown, setMarkdown }) => {
     return (
         <section className="markdown">
             <textarea onChange={(e) => {
                 setMarkdown(e.target.value)
             }} value={markdown} className="input">{markdown}</textarea>
-            <article className="result">{markdown}</article>
+            <article className="result">
+                <ReactMarkdown>
+                    {markdown}
+                </ReactMarkdown>
+
+            </article>
         </section>
     )
 }
